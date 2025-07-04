c15c383b02f9410a6092ecbe10ee2a605bb34379
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-07-04 19:20:34 +0300

    created user entity

---
+++ src/main/java/com/viberato/aldio/entity/User.java
@@ -0,0 +1,52 @@
+package com.viberato.aldio.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table( name = "users")
+@Getter @Setter
+@ToString
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long userID;
+
+    @Column(name = "username", nullable = false, unique = true, length = 100)
+    private String username;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "second_name")
+    private String secondName;
+
+    @Column
+    private LocalDateTime timestamp;
+
+    @PrePersist
+    protected void onCreate(){
+        if(this.timestamp == null){
+            this.timestamp = LocalDateTime.now();
+        }
+    }
+
+    public User() {
+    }
+
+    public User(String username, String passwordHash, String firstName, String secondName) {
+        this.username = username;
+        this.passwordHash = passwordHash;
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.timestamp = LocalDateTime.now();
+    }
+}
