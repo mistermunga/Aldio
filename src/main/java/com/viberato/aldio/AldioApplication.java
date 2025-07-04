7178e55d6d323ccd2d1132b1f20cc0fe7dcc27d4
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-07-04 18:56:09 +0300

    Initial commit after setup

---
+++ src/main/java/com/viberato/aldio/AldioApplication.java
@@ -0,0 +1,13 @@
+package com.viberato.aldio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AldioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AldioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/viberato/aldio/AldioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viberato.aldio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AldioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
