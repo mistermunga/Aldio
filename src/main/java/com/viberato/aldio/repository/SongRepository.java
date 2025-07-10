3c91a5a944367700ff8f83695b9ce8280509042c
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-07-10 19:16:44 +0300

    added a couple of useful queries

--- src/main/java/com/viberato/aldio/repository/SongRepository.java
+++ src/main/java/com/viberato/aldio/repository/SongRepository.java
@@ -4,7 +4,13 @@ import com.viberato.aldio.entity.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface SongRepository extends JpaRepository<Song, Long> {
+    Optional<Song> findByFilepath(String filepath);
 
+    List<Song> findByTitle(String title);
+    List<Song> findByArtist(String artist);
 }
